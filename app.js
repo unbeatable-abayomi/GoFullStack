76d87dd9df4e80b4a557eb367d31b20daad2ccb5
abayomi igwubor <igwubor@outlook.com>
2019-10-17 12:19:38 +0100

    modifiedDatabase

--- app.js
+++ app.js
@@ -42,29 +42,89 @@ app.use((req, res, next) => {
        price: req.body.price,
        userId: req.body.userId
      });
-
+    thing.save().then( 
+      () =>{
+        res.status(201).json({
+          message: "Post Saved Successfully!"
+        })
+      }
+    ).catch(
+      (error)=>{
+res.status(400).json({
+  error: error
+});
+      }
+    );
   });
 
+app.get('/api/stuff/:id', (req,res,next)=>{
+Thing.findOne({
+  _id: req.params.id
+}).then(
+  (thing) => {
+    res.status(200).json(thing)
+  }
+).catch(
+  (error)=>{
+    res.status(404).json({
+      error: error
+    });
+  }
+);
+});
+app.put('/api/stuff/:id', (req, res, next)=> {
+  const thing = new Thing ({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  Thing.updateOne({_id: req.params.id}, thing).then(
+    ()=>{
+      res.status(201).json({
+        message: "Thing Updated Sucessfully"
+      })
+    }
+  ).catch(
+    (error)=>{
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+
+app.delete('/api/stuff/:id', (req, res, next)=>{
+    Thing.deleteOne({_id: req.params.id}).then(
+      ()=>{
+        res.status(200).json({
+          message: 'Deleted!'
+        });
+      }
+    ).catch(
+      (error)=>{
+     res.status(400).json({
+       error: error
+     });
+      }
+    );
+});
+
 app.use( '/api/stuff',(req, res , next) => {
-    const stuff =[ 
-        {
-        _id: 'frenzy',
-        title: 'My First Thing',
-        description: 'All the info about my first thing',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/e/e3/Canon_EOS_60D_01.jpg',
-        price: 49,
-        userId: 'random'
-      },
-      {
-        _id: 'frenzyTwo',
-        title: 'My Second Thing',
-        description: 'All the info about my second thing',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/e/e3/Canon_EOS_60D_01.jpg',
-        price: 249,
-        userId: 'randomTwo'
-      },
-    ];
-    res.status(200).json(stuff)
+   Thing.find().then(
+     (things)  => {
+       res.status(200).json(things);
+     }
+   ).catch(
+   (error)=>{
+      res.status(400).json({
+        error: error
+      });
+   }
+   );
 });
 
 module.exports = app;
